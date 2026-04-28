16f875e6921e16201403b170938b4388c0c5d545
evchip <evchip@pm.me>
2026-04-27 20:57:17 -0700

    add charge intent with stripe method

--- src/lib/mpp.ts
+++ src/lib/mpp.ts
@@ -1,17 +1,21 @@
 // MPP server setup for our service
 // Docs: https://mpp.dev | SDK: https://github.com/wevm/mppx
 
-import { USDC } from "./tempo";
+import Stripe from "stripe"
+import { USDC } from "./tempo"
 
 export function createMppServer(recipientAddress: `0x${string}`) {
-  return import("mppx/server").then(({ Mppx, tempo }) =>
-    Mppx.create({
+  return import("mppx/server").then(({ Mppx, tempo, stripe }) => {
+    const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!)
+    return Mppx.create({
       methods: [
-        tempo({
-          currency: USDC,
-          recipient: recipientAddress,
+        tempo({ currency: USDC, recipient: recipientAddress }),
+        stripe.charge({
+          client: stripeClient,
+          networkId: process.env.STRIPE_NETWORK_ID!,
+          paymentMethodTypes: ["card"],
         }),
       ],
     })
-  );
+  })
 }
